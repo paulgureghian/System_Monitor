ed9b72d0dee25b46f576deac5a784abbe96d0c3b
paulgureghian <paulgureghian@att.net>
2020-07-06 16:01:11 -0700

    Added logic to 'Uptime()'

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -100,7 +100,22 @@ float LinuxParser::MemoryUtilization() {
 }
 
 // TODO: Read and return the system uptime
-long LinuxParser::UpTime() { return 0; }
+long LinuxParser::UpTime() { 
+  
+    string line;
+    string value = " ";
+
+    std::ifstream filestream(kProcDirectory + kUptimeFilename);
+
+    if (filestream.is_open()) {
+      
+      std::getline(filestream, line);
+      std::istringstream linestream(line);
+      linestream >> value;
+    }  
+  
+    return (std::stol(value)); 
+}
 
 // TODO: Read and return the number of jiffies for the system
 long LinuxParser::Jiffies() { return 0; }
