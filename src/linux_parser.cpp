8aedd8c9813d42c8a79f25c1213844ad77188115
paulgureghian <paulgureghian@att.net>
2020-07-07 15:50:53 -0700

    Added logic to 'TotalProcesses()'

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -235,7 +235,27 @@ vector<string> LinuxParser::CpuUtilization() {
   }
 
 // TODO: Read and return the total number of processes
-int LinuxParser::TotalProcesses() { return 0; }
+int LinuxParser::TotalProcesses() { 
+  
+    string line = " ";
+    string key = " ";
+    string value = " ";  
+  
+    std::ifstream filestream(kProcDirectory + kStatFilename);
+
+    if (filestream.is_open()) {
+      while (std::getline(filestream, line)) {
+
+        std::istringstream linestream(line);
+        linestream >> key >> value;
+
+        if (key == "processes")
+          return std::stoi(value);    
+      }
+    }   
+   
+    return 0; 
+}
 
 // TODO: Read and return the number of running processes
 int LinuxParser::RunningProcesses() { return 0; }
