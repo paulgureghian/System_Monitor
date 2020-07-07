67aa41ef055a47cf2d06a419b7e2d936370e3faa
paulgureghian <paulgureghian@att.net>
2020-07-07 13:39:00 -0700

    Added logic to 'IdleJIffies()'

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -191,7 +191,27 @@ long LinuxParser::ActiveJiffies() {
 }
 
 // TODO: Read and return the number of idle jiffies for the system
-long LinuxParser::IdleJiffies() { return 0; }
+long LinuxParser::IdleJiffies() { 
+  
+    string line = " ";
+    string cpu = " ";
+
+    string sUser = " ",sNice = " ", sSystem = " ", sIdle = " ", sIOwait = " ",
+           sIRQ = " ", sSoftIRQ = " ", sSteal = " ", sGuest = " ", sGuestNice = " ";  
+  
+  std::ifstream filestream(kProcDirectory + kStatFilename);
+
+  if (filestream.is_open()) {
+
+    std::getline(filestream, line);
+    std::istringstream linestream(line);
+
+    linestream >> cpu >> sUser >> sNice >> sSystem >> sIdle >> sIOwait >> sIRQ
+               >> sSoftIRQ >> sSteal >> sGuest >> sGuestNice;   
+  } 
+  
+  return (std::stol(sIdle) + std::stol(sIOwait)); 
+}
 
 // TODO: Read and return CPU utilization
 vector<string> LinuxParser::CpuUtilization() { return {}; }
