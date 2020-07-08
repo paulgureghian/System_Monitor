737d7b7148a3c261114372b43994c729d773c239
paulgureghian <paulgureghian@att.net>
2020-07-08 13:33:01 -0700

    Added public function 'Process()'

--- include/process.h
+++ include/process.h
@@ -16,6 +16,8 @@ class Process {
   std::string Ram();                       // TODO: See src/process.cpp
   long int UpTime();                       // TODO: See src/process.cpp
   bool operator<(Process const& a) const;  // TODO: See src/process.cpp
+  bool operator>(Process const& a) const;
+  Process(int pid);
 
   // TODO: Declare any necessary private members
  private:
