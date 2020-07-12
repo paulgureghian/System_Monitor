049b42dc4250162a0b59dc9ecd271099919f1b39
paulgureghian <paulgureghian@att.net>
2020-07-12 16:27:43 -0700

    Declared private members

--- include/processor.h
+++ include/processor.h
@@ -1,12 +1,16 @@
 #ifndef PROCESSOR_H
 #define PROCESSOR_H
 
+#include "linux_parser.h"
+
 class Processor {
  public:
   float Utilization();  // TODO: See src/processor.cpp
 
   // TODO: Declare any necessary private members
  private:
+    int tAll_ = 0;
+    int tIdle = 0;
 };
 
-#endif
+#endif  
