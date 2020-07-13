c230050b4c91aadb8d9d24a1c5d3d75975d46366
paulgureghian <paulgureghian@att.net>
2020-07-12 18:36:38 -0700

    Small edit

--- include/processor.h
+++ include/processor.h
@@ -5,12 +5,13 @@
 
 class Processor {
  public:
-  float Utilization();  // TODO: See src/processor.cpp
+  Processor();  
+  float Utilization();  
 
-  // TODO: Declare any necessary private members
+  //  Declare private members
  private:
     int tAll_ = 0;
-    int tIdle = 0;
+    int tIdle_ = 0;
 };
 
 #endif  
