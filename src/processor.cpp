ca97984bcb9cdf67c100107b973cbe7450cd27bf
paulgureghian <paulgureghian@att.net>
2020-07-12 18:30:33 -0700

    Added logic to Utilization()

--- src/processor.cpp
+++ src/processor.cpp
@@ -1,4 +1,20 @@
 #include "processor.h"
 
-// TODO: Return the aggregate CPU utilization
-float Processor::Utilization() { return 0.0; }
+Processor::Processor() {
+
+    tAll_ = LinuxParser::Jiffies();
+    tIdle_ = LinuxParser::IdleJiffies();
+}
+
+// Return the aggregate CPU utilization
+float Processor::Utilization() { 
+    
+    float tAllOLd = tAll_;
+    float tIdleOld = tIdle_;        
+
+    tAll_ = LinuxParser::Jiffies();
+    tIdle_ = LinuxParser::IdleJiffies();    
+    
+    float cpu_utilization = ((( tAll_ - tAllOLd) - (tIdle_ - tIdleOld)) / (tAll_ - tAllOLd));
+    return (cpu_utilization > 0.0) ? cpu_utilization : 0.0; 
+}
